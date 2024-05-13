b69d9b9fb640ad8842c98f9dcd8523d835ebea48
Aleksey Ivanyukov <aa.ivanyukov@gmail.com>
2024-05-13 20:34:59 +0400

    step 1

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,11 +1,13 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
+
 const program = new Command();
 
 program
-  .description('Compares two configuration files and shows a difference.');
-  .version('0.1.0');
-  .option();
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1', '-V, --version', 'output the version number');
+//  .option('-f ');
 
 program.parse();
