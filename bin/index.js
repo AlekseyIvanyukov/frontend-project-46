25d50ef01a9ed28e0fdc5a27acb61089dbea803f
Aleksey Ivanyukov <aa.ivanyukov@gmail.com>
2024-06-04 09:52:34 +0400

    step 8

--- bin/index.js
+++ bin/index.js
@@ -1,8 +1,7 @@
 #!/usr/bin/env node
 
-// eslint-disable-next-line import/extensions
 import genDiff from '../index.js';
 
-genDiff(__fixtures__/file1.json, __fixtures__/file2.json);
+genDiff();
 
 export default genDiff;
