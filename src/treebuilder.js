39f232c79390ea8840a8954069cd863501b933d8
Aleksey Ivanyukov <aa.ivanyukov@gmail.com>
2024-06-06 11:25:14 +0400

    step 9

--- src/treebuilder.js
+++ src/treebuilder.js
@@ -1,4 +1,4 @@
-import _ from 'lodash';
+/* import _ from 'lodash';
 
 const buildTree = (data1, data2) => {
   const keys1 = Object.keys(data1);
@@ -17,6 +17,35 @@ const buildTree = (data1, data2) => {
         status: 'updated', key, oldValue: data1[key], newValue: data2[key],
       };
     } return { status: 'unchanged', key, value: data1[key] };
+  });
+};
+
+export default buildTree;
+ */
+
+import _ from 'lodash';
+
+const buildTree = (data1, data2) => {
+  const keys1 = Object.keys(data1);
+  const keys2 = Object.keys(data2);
+  const unionSortedKeys = _.sortBy(_.union(keys1, keys2));
+
+  return unionSortedKeys.map((key) => {
+    let result;
+    if (!_.has(data1, key)) {
+      result = { status: 'added', key, value: data2[key] };
+    } else if (!_.has(data2, key)) {
+      result = { status: 'deleted', key, value: data1[key] };
+    } else if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
+      result = { status: 'parent', key, children: buildTree(data1[key], data2[key]) };
+    } else if (!_.isEqual(data1[key], data2[key])) {
+      result = {
+        status: 'updated', key, oldValue: data1[key], newValue: data2[key],
+      };
+    } else {
+      result = { status: 'unchanged', key, value: data1[key] };
+    }
+    return result;
   });
 };
 
